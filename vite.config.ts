eba16f27b9e340aedc115b445469e6fef3fee4b0
Nekane <montielarce9@gmail.com>
2024-11-21 13:17:39 +0100

    A

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+// vite.config.js
+export default {
+  server: {
+    port: 5174  // Cambiar al puerto deseado
+  }
+}
+
